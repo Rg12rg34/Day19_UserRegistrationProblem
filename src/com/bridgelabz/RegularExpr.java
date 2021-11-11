7357828426bc7fea32cbdf2869190e80d2a6b015
Rg12rg34 <rahulguptamca1@gmail.com>
2021-11-11 19:46:26 +0530

    Adding User Registration Format For Email ID Validation

--- src/com/bridgelabz/RegularExpr.java
+++ src/com/bridgelabz/RegularExpr.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class RegularExpr {
     public void firstName() {
         Scanner sc=new Scanner(System.in);
-        System.out.println("The First Name is: ");
+        System.out.println("Enter First Name is: ");
         String fname=sc.nextLine();
         String regexFname = "^[A-Z]{1}[a-z]{2,}";
 
@@ -19,11 +19,11 @@ public class RegularExpr {
 
     public void lastName() {
         Scanner sc=new Scanner(System.in);
-        System.out.println("The Last Name is: ");
+        System.out.println("Enter Last Name is: ");
         String lname=sc.nextLine();
-        String regexLname = "^[A-Z]{1}[a-z]{2,}";
+        String regexFname = "^[A-Z]{1}[a-z]{2,}";
 
-        if(lname.matches(regexLname)==true)
+        if(lname.matches(regexFname)==true)
         {
             System.out.println("The last name "+lname+" is valid.");
         }else
@@ -31,10 +31,26 @@ public class RegularExpr {
             System.out.println("The last name "+lname+" is invalid.");
         }
     }
+
+    public void eMail() {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter eMail is: ");
+        String email=sc.nextLine();
+        String regexFname = "^[A-Za-z0-9]+([._+-][A-Za-z0-9]+)*@[A-Za-z0-9]+.[A-Za-z]{2,4}([.][A-Za-z]{2})*$";
+
+        if(email.matches(regexFname)==true)
+        {
+            System.out.println("The eMail "+email+" is valid.");
+        }else
+        {
+            System.out.println("The eMail "+email+" is invalid.");
+        }
+    }
     public static void main(String[] args) {
         System.out.println("Welcome to the user registration.");
         RegularExpr obj=new RegularExpr();
         obj.firstName();
         obj.lastName();
+        obj.eMail();
     }
 }
